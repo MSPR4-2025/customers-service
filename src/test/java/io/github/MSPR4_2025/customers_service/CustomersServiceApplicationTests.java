7fd3bc3ef00c7f126b43515f3481ab55dc712d98
celeri <celestin.bouchet@gmail.com>
2025-06-11 08:30:14 +0200

    Remove failing test

--- src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
+++ src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
@@ -3,18 +3,11 @@ package io.github.MSPR4_2025.customers_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @SpringBootTest
 class CustomersServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 		
-	}
-
-	@Test
-	void thisShouldFail() {
-		assertThat(false).isTrue();
 	}
 }
