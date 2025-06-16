cec1cb0f5116d301919a83e79f9e812065547a6c
celeri <celestin.bouchet@gmail.com>
2025-06-16 09:06:41 +0200

    Remove failing test

--- src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
+++ src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package io.github.MSPR4_2025.customers_service;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,10 +11,5 @@ class CustomersServiceApplicationTests {
     @Test
     void contextLoads() {
 
-    }
-
-    @Test
-    void testShouldFail() {
-        Assertions.assertThat(false).isTrue();
     }
 }
