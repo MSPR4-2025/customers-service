44b98465ea51f08d3ee4ffe89c8ef4ddf9dfffb2
celeri <celestin.bouchet@gmail.com>
2025-06-16 09:04:03 +0200

    Add failing test

--- src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
+++ src/test/java/io/github/MSPR4_2025/customers_service/CustomersServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package io.github.MSPR4_2025.customers_service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,5 +12,10 @@ class CustomersServiceApplicationTests {
     @Test
     void contextLoads() {
 
+    }
+
+    @Test
+    void testShouldFail() {
+        Assertions.assertThat(false).isTrue();
     }
 }
