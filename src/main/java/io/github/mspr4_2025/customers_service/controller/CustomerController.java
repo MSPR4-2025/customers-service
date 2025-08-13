41cc2b5d77aeef2714dfd73a2033cb6e4ba4e656
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-08-13 18:45:43 +0200

    Revert "implementation de rabbitMQ"
    This reverts commit 2f5a0e0ddb8491b1b961d165d684756b9692fafb.

--- src/main/java/io/github/mspr4_2025/customers_service/controller/CustomerController.java
+++ src/main/java/io/github/mspr4_2025/customers_service/controller/CustomerController.java
@@ -8,12 +8,6 @@ import io.github.mspr4_2025.customers_service.model.CustomerDto;
 import io.github.mspr4_2025.customers_service.model.CustomerUpdateDto;
 import io.github.mspr4_2025.customers_service.service.CustomerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
@@ -28,46 +22,16 @@ import java.util.UUID;
 public class CustomerController {
     private final CustomerService customerService;
     private final CustomerMapper customerMapper;
-    private final RabbitTemplate rabbitTemplate;
-
-
-    @Value("events-exchange")
-    private String eventsExchange;
-
-    @Value("customers-topic-exchange")
-    private String customersExchange;
-
-    @Value("customers.update")
-    private String customerUpdateRouting;
-
-    @Value("${app.rabbitmq.routingkey:customers.get.all}")
-    private String customersGetAllRouting;
-    
-    @Value("customers.get")
-    private String customersGetRouting;
-
-    @Value("customers.create")
-    private String customersCreateRouting;
-    
-    @Value("customers.delete")
-    private String customersDeleteRouting;
-
-
-
 
     @GetMapping("/")
     public ResponseEntity<List<CustomerDto>> listCustomers() {
         List<CustomerEntity> customerEntities = customerService.getAllCustomers();
-        rabbitTemplate.convertAndSend(customersExchange, customersGetAllRouting, "GET /api/customers/ called");
-        rabbitTemplate.convertAndSend(eventsExchange, customersGetAllRouting, "GET /api/customers/ called");
-
 
         return ResponseEntity.ok(customerMapper.fromEntities(customerEntities));
     }
 
     @PostMapping("/")
     public ResponseEntity<Void> createCustomer(@RequestBody CustomerCreateDto customerCreate) {
-        
         CustomerEntity customerEntity = customerService.createCustomer(customerCreate);
 
         // Get the url to GET the created customer
@@ -78,16 +42,12 @@ public class CustomerController {
             .build()
             .toUri();
 
-        rabbitTemplate.convertAndSend(customersExchange, customersCreateRouting, customerMapper.fromEntity(customerEntity).toString());
         return ResponseEntity.created(customerUri).build();
-        
     }
 
     @GetMapping("/{uid}")
     public ResponseEntity<CustomerDto> getCustomerByUid(@PathVariable UUID uid) {
         CustomerEntity customerEntity = customerService.getCustomerByUid(uid);
-        
-        rabbitTemplate.convertAndSend(customersExchange, customersGetRouting, customerMapper.fromEntity(customerEntity).toString());
 
         return ResponseEntity.ok(customerMapper.fromEntity(customerEntity));
     }
@@ -95,10 +55,7 @@ public class CustomerController {
     @PutMapping("/{uid}")
     public ResponseEntity<Void> updateCustomer(@PathVariable UUID uid,
                                                @RequestBody CustomerUpdateDto customerUpdate) {
-        // customerService.updateCustomer(uid, customerUpdate);
-        System.out.println("Customer with UID " + uid + " updated successfully. customersUpdateRouting: " + customerUpdateRouting);
-        customerMapper.updateEntityFromDto(customerUpdate, customerService.getCustomerByUid(uid));
-        rabbitTemplate.convertAndSend(customersExchange, customerUpdateRouting, uid.toString());
+        customerService.updateCustomer(uid, customerUpdate);
 
         return ResponseEntity.ok().build();
     }
@@ -106,28 +63,7 @@ public class CustomerController {
     @DeleteMapping("/{uid}")
     public ResponseEntity<Void> deleteCustomer(@PathVariable UUID uid) {
         customerService.deleteCustomer(uid);
-        
-        rabbitTemplate.convertAndSend(customersExchange, customersDeleteRouting, uid.toString());
 
         return ResponseEntity.ok().build();
-    }
-    
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value = "customers-queue", durable = "true"),
-            exchange = @Exchange(value = "orders-exchange", type = "topic"),
-            key = "customers.get"
-    ))
-    public void handleGetCustomerRequest(String message){
-        
-        
-        try{
-            Thread.sleep(1000); // Simulate processing delay
-            System.out.println("handleGetCustomerRequest Received message: " + message);
-
-            CustomerCreateDto customerCreateDto = new CustomerCreateDto();
-            customerService.createCustomer(customerCreateDto);
-        } catch (Exception e) {
-            System.err.println("Error processing message: " + e.getMessage());
-        }
     }
 }
--- src/main/java/io/github/mspr4_2025/customers_service/model/CustomerDto.java
+++ src/main/java/io/github/mspr4_2025/customers_service/model/CustomerDto.java
@@ -2,13 +2,12 @@ package io.github.mspr4_2025.customers_service.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
-@ToString
 public class CustomerDto extends CustomerUpdateDto {
     private UUID uid;
 
