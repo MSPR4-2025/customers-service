d28d1167aef424f7d4aca30fe3a59b1f5b6f5f24
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-09-03 22:32:53 +0200

    Final working version - handle created order

--- src/main/java/io/github/mspr4_2025/customers_service/service/CustomerService.java
+++ src/main/java/io/github/mspr4_2025/customers_service/service/CustomerService.java
@@ -6,19 +6,61 @@ import io.github.mspr4_2025.customers_service.model.CustomerCreateDto;
 import io.github.mspr4_2025.customers_service.model.CustomerUpdateDto;
 import io.github.mspr4_2025.customers_service.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final RabbitTemplate rabbitTemplate;
+
+
+    @Value("order_events_exchange")
+    private String orderEventsExchange;
+
+    @Value("stock_confirmation_queue")
+    private String stockConfirmationQueue;
+
+    @Value("stock_check_queue")
+    private String stockCheckQueue;
+
+    @Value("customer_verification_queue")
+    private String customerVerificationQueue;
+
+    @Value("customer_confirmation_queue")
+    private String customerConfirmationQueue;
+
+    @Value("order.created")
+    private String orderCreatedKey;
+
+    @Value("customer.verification.requested")
+    private String customerVerificationRequestedKey;
+    
+    @Value("customer.verification.confirmed")
+    private String customerVerificationConfirmedKey;
+
+    @Value("product.verification.requested")
+    private String productVerificationRequestedKey;
+
+    @Value("product.verification.confirmed")
+    private String productVerificationConfirmedKey;
 
     public List<CustomerEntity> getAllCustomers() {
         return customerRepository.findAll();
@@ -56,5 +98,62 @@ public class CustomerService {
         CustomerEntity entity = this.getCustomerByUid(uid);
 
         customerRepository.delete(entity);
+    }
+
+    @RabbitListener(
+        bindings = @QueueBinding(
+            value = @Queue(value = "customer_verification_queue"),
+            exchange = @Exchange(value = "order_events_exchange", type="topic"),
+            key = "order.created"
+        )
+    )
+    public void handleCreateOrderMessage(String message) {
+        try {
+            log.info("message received, creating order. Message: " + message);
+            System.out.println("creating order");
+
+            JsonNode orderNode = objectMapper.readTree(message);
+            JsonNode customerNode = orderNode.get("order").get("customerUid");
+            JsonNode orderUidNode = orderNode.get("orderUid");
+
+            if (customerNode == null  || orderUidNode == null ) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid message");
+            }
+
+            log.info("parsed customerUid: " + customerNode.asText());
+            
+            try{
+                customerRepository.findByUid(UUID.fromString(customerNode.asText()));
+            } catch (Exception ex) {
+            log.error("Exception in handleCreateOrderMessage: " + ex.getMessage());
+            
+            
+            if (orderUidNode != null) {
+                log.info("orderUidNode != null");
+                
+                sendOrderStatus(UUID.fromString(orderUidNode.asText()), "FAILED");
+            }
+        }
+            UUID orderUid = UUID.fromString(orderUidNode.asText());
+            sendOrderStatus(orderUid, "CONFIRMED");
+
+        } catch (Exception e) {
+            // throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+            log.error("CustomerService.handleCreateOrderMessage exception: " + e.getMessage());
+        }
+    }
+
+
+    public void sendOrderStatus(UUID orderUid, String customerCheckStatus){
+        try{
+            String confirmationJson = objectMapper.createObjectNode()
+                            .put("orderUid", orderUid.toString())
+                            .put("customerCheckStatus", customerCheckStatus)
+                            .toString();
+            log.info("sendOrderStatus message sent : " + confirmationJson);
+            rabbitTemplate.convertAndSend(orderEventsExchange, customerVerificationConfirmedKey, confirmationJson );
+        } catch(Exception ex) {
+            log.info("productServices.sendOrderStatus exception: " + ex);
+        }
     }
 }
