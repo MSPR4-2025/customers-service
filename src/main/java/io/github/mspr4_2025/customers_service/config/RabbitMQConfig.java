bd28005ede1e67344be132b76e89f4d85d03c029
celeri <celestin.bouchet@gmail.com>
2025-09-04 14:42:57 +0200

    fix rabbitmq connection

--- src/main/java/io/github/mspr4_2025/customers_service/config/RabbitMQConfig.java
+++ src/main/java/io/github/mspr4_2025/customers_service/config/RabbitMQConfig.java
@@ -6,7 +6,6 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.*;
 import org.springframework.context.annotation.Bean;
@@ -30,12 +29,6 @@ public class RabbitMQConfig {
     public static final String productVerificationConfirmedKey = "product.verification.confirmed";
 
 
-
-
-    @Bean
-    public CachingConnectionFactory cf() {
-        return new CachingConnectionFactory("host.docker.internal", 5672);
-    }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory cf) {
